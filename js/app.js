db167508f4e5ebc7d60c6d5a558f1da785a35d36
Marta <marta.anthony1@gmail.com>
2020-04-28 18:17:35 -0700

    js changes

--- js/app.js
+++ js/app.js
@@ -3,28 +3,64 @@
 // greeting
 alert('Oh hello!');
 var name = prompt('What\'s your name, beautiful?');
-console.log('User\'s name is' + name);
+// console.log('User\'s name is ' + name);
 alert('Welcome to my page, ' + name + '!');
 
-// Invite to play
+// Invitation to play
 var playGame = prompt('Do you want to play a guessing game with me?')
 
 if (playGame.toLowerCase() === 'yes'){
-  alert('Cool! I will ask you a couple of questions, and you\'ll answer yes or no. Let\'s go!');
+  alert('Cool! I will ask you a couple of questions, and you\'ll answer yes or no, or if you\'re lazy, just say y or n. Let\'s go!');
 } else {
-  alert('Maybe another time!')
+   alert('Maybe another time!'); 
 }
 
 // Who's my husband?
 var husband = prompt('Is my husband a military trained killing machine?')
 
-if (husband.toLowerCase() === 'yes'){
-  alert('Yup! Your\'re right. My husband was a captain in the US Marine Corps.')
+if (husband.toLowerCase() === 'yes' || husband.toLowerCase() === 'y'){
+  // console.log('correct');
+  alert('Yup! Your\'re right. My husband was a captain in the US Marine Corps.');
+  
 } else { 
-  alert('Oh yes he is! He is a badass US Marine Corps officer.')
+  // console.log('incorrect');
+  alert('Oh yes he is! He is a badass US Marine Corps officer.');
 }
 
-  
+// Did I see a polar bear?
+var bear = prompt('Did I see a polar bear on the street of my hometown in Poland one winter?')
+
+if (bear.toLowerCase() === 'yes' || bear.toLowerCase() === 'y'){
+  // console.log('incorrect');
+  alert('No, you silly! Poland is not Siberian tundra, there are no polar bears on the streets.');
+} else {
+  // console.log('correct');
+  alert('Bravo! That would be a scary and improbable situation.');
+}
+
+// Do I like veggies? 
+var favoriteFood = prompt('Is my favorite food group veggies?')
+
+if (favoriteFood.toLowerCase() === 'yes' || favoriteFood.toLowerCase() === 'y'){
+  // console.log('correct');
+  alert('You bet! I LOVE veggies. I\'m practically a bunny rabbit.');
+} else {
+  // console.log('incorrect');
+  alert('Why do you say that? Do you think I\'m fat? Of course I like veggies.');
+}
+
+var age = prompt('Am I 33?')
+
+if (age.toLowerCase() === 'yes' || age.toLowerCase() === 'y'){
+  // console.log('incorrect');
+  alert('Guess again. Just kidding, I\'m not going to tell you my age anyway.');
+} else {
+  // console.log('correct');
+  alert("Doesn't matter, I won't tell you my age anyway.");
+}
+
+alert(name +', thank you so much for playing with me. Now you know my secrets.')
+
 
 
 
