634fb30c16ce29a76745171ed5deea82e531b6e0
jonathon lee <jonnyleealas@gmail.com>
2020-04-30 15:57:45 -0700

    new functions

--- js/app.js
+++ js/app.js
@@ -2,122 +2,156 @@
 
 // greeting
 alert('Oh hello!');
-var name = prompt('What\'s your name, beautiful?');
+var userName = prompt('What\'s your name, beautiful?');
 // console.log('User\'s name is ' + name);
-alert('Welcome to my page, ' + name + '!');
+alert('Welcome to my page, ' + userName + '!');
 
 var answers = [];
+function husbandQuestion(){
+  var husband = prompt('Is my husband a military trained killing machine?'); 
+  if(husband.toLowerCase() === 'yes' || husband.toLowerCase() === 'y'){
+    // console.log('correct annswer - my husband is a killing machine');
+    answers.push(1);
+    alert('Yup! Your\'re right. My husband was a captain in the US Marine Corps.');
+  } else {
+    //  console.log('incorrect');
+    alert('Oh yes he is! He is a badass US Marine Corps officer.');
+  }
+}
+function bearQuestion(){
+
+  var bear = prompt('Did I see a polar bear on the street of my hometown in Poland one winter?');
+
+  if(bear.toLowerCase() === 'yes' || bear.toLowerCase() === 'y'){
+  // console.log('incorrect');
+    alert('No, you silly! Poland is not a Siberian tundra, there are no polar bears on the streets.');
+  } else {
+    // console.log('correct - there are no polar bears in Poland');
+    answers.push(1);
+    alert('Bravo! That would be a scary proposition. And totally messed up, since there are no polar bears outside of the zoo in Poland');
+  }
+}
+function veggieQuestion(){
+  var favoriteFood = prompt('Is my favorite food group veggies?');
+
+  if(favoriteFood.toLowerCase() === 'yes' || favoriteFood.toLowerCase() === 'y'){
+    // console.log('correct - my favorite foods are veggies');
+    answers.push(1);
+    alert('You bet! I LOVE veggies. I\'m practically a bunny rabbit.');
+  } else {
+    // console.log('incorrect');
+    alert('Why do you say that? Do you think I\'m fat? Of course I like veggies.');
+  }
+}
+function danceQuestion(){
+
+  var dance = prompt('Did I use to dance salsa competitevely?');
+
+  if(dance.toLowerCase() === 'yes' || dance.toLowerCase() === 'y'){
+    // console.log('incorrect');
+    alert('Heck no, I am the most uncoordinated person ever.');
+  } else {
+    // console.log('correct - I can\'t dance');
+    answers.push(1);
+    alert('Very good. I am not a dancer');
+  }
+}
+function plantQuestion(){
+  for (var i=0; i<3; i++){
+    var number = prompt('How many plants did I accidentally kill last year? Choose a number from 1 to 10. You have 3 guesses!');
+    if(parseInt(number) === 3){
+    // console.log('correct - 3 plants killed');
+      answers.push(1);
+      alert('Good job! You are figuring me out.');
+      break;
+    } else if (number > 3){
+      // console.log('incorrect');
+      alert('Hold your horses, my black thumb is not that bad.');
+    } else {
+      console.log(0);
+      alert('I wish. It was definitely more.');
+    }
+  }
+}
+function sweetsQuestion(){
+  alert('One more question, ok? I have a very sweet tooth, and I am wondering if you can guess which treat is my favorite.');
+
+  for (var i=0; i<6; i++){
+    var favoriteSweets = ['chocolate truffles', 'peanut butter cups', 'flan', 'hard candy', 'salted carmels', 'cookies', 'donuts', 'bananas'];
+    var sweetGuess = prompt('What do I like most: chocolate truffles, peanut butter cups, flan, hard candy, salted carmels, cookies, donuts, or bananas?');
+    if(sweetGuess === favoriteSweets[4]) {
+    //  console.log('correct - salted carmels are my favorite');
+      answers.push(1);
+      alert('You got it! I would kill for salted carmels right now.');
+      break;
+    } else {
+    //  console.log('incorrect');
+      alert('That\'s not it.');
+    }
+  }
+}
+function ageQuestion(){
+  var age = prompt('Am I 33?');
+
+  if(age.toLowerCase() === 'yes' || age.toLowerCase() === 'y'){
+    // console.log('incorrect');
+    alert('Guess again. Just kidding, I\'m not going to tell you my age anyway.');
+  } else {
+    // console.log('correct - I am not 33');
+    answers.push(1);
+    alert('Doesn\'t matter, I won\'t tell you my age anyway.');
+  }
+}
 
 // Invitation to play
-var playGame = prompt('Do you want to play a guessing game with me?')
+function gameInvite(){
+  var playGame = prompt('Do you want to play a guessing game with me?');
 
-if(playGame.toLowerCase() === 'yes'){
-  alert('Cool! I will ask you a couple of questions, and you\'ll answer yes or no, or if you\'re lazy, just say y or n. Let\'s go!')
+  if(playGame.toLowerCase() === 'yes'){
+    alert('Cool! I will ask you a couple of questions, and you\'ll answer yes or no, or if you\'re lazy, just say y or n. Let\'s go!');
+    husbandQuestion();
+    bearQuestion();
+    ageQuestion();
+    veggieQuestion();
+    danceQuestion();
+    plantQuestion();
+    sweetsQuestion();
 
-  // Who's my husband?
-var husband = prompt('Is my husband a military trained killing machine?')
+// Who's my husband?
 
-if(husband.toLowerCase() === 'yes' || husband.toLowerCase() === 'y'){
-  // console.log('correct annswer - my husband is a killing machine');
-  answers.push(1);
-  alert('Yup! Your\'re right. My husband was a captain in the US Marine Corps.');
-  
-} else { 
-  //  console.log('incorrect');
-  alert('Oh yes he is! He is a badass US Marine Corps officer.');
-}
+//husbandQuestion();
 
-// Did I see a polar bear?
-var bear = prompt('Did I see a polar bear on the street of my hometown in Poland one winter?')
+//Did I see a polar bear?
 
-if(bear.toLowerCase() === 'yes' || bear.toLowerCase() === 'y'){
-  // console.log('incorrect');
-  alert('No, you silly! Poland is not a Siberian tundra, there are no polar bears on the streets.');
-} else {
-  // console.log('correct - there are no polar bears in Poland');
-  answers.push(1);
-  alert('Bravo! That would be a scary proposition. And totally messed up, since there are no polar bears outside of the zoo in Poland');
-}
+  //bearQuestion();
 
-// Do I like veggies? 
-var favoriteFood = prompt('Is my favorite food group veggies?')
+//   // Do I like veggies? 
 
-if(favoriteFood.toLowerCase() === 'yes' || favoriteFood.toLowerCase() === 'y'){
-  // console.log('correct - my favorite foods are veggies');
-  answers.push(1);
-  alert('You bet! I LOVE veggies. I\'m practically a bunny rabbit.');
-} else {
-  // console.log('incorrect');
-  alert('Why do you say that? Do you think I\'m fat? Of course I like veggies.');
-}
+//veggieQuestion();
+//   // what's my age
 
-// what's my age
+  //ageQuestion();
 
-var age = prompt('Am I 33?')
+  //   //Am I a dancer
 
-if(age.toLowerCase() === 'yes' || age.toLowerCase() === 'y'){
-  // console.log('incorrect');
-  alert('Guess again. Just kidding, I\'m not going to tell you my age anyway.');
-} else {
-  // console.log('correct - I am not 33');
-  answers.push(1);
-  alert('Doesn\'t matter, I won\'t tell you my age anyway.');
-}
+  //danceQuestion();
+//   // How many plants did I kill
 
-//Am I a dancer
+//plantQuestion();
 
-var dance = prompt('Did I use to dance salsa competitevely?')
+  // My favorite sweets
 
-if(dance.toLowerCase() === 'yes' || dance.toLowerCase() === 'y'){
-  // console.log('incorrect');
-  alert('Heck no, I am the most uncoordinated person ever.');
-} else {
-  // console.log('correct - I can\'t dance');
-  answers.push(1);
-  alert('Very good. I am not a dancer');
-}
+   //sweetsQuestion();
 
-// How many plants did I kill
+    //   //  console.log(answers);
+    alert('You\'ve got ' + answers.length + ' correct answers out of 7!');
 
-for (var i=0; i<3; i++){ 
-  var number = prompt('How many plants did I accidentally kill last year? Choose a number from 1 to 10. You have 3 guesses!');
-if(number === "3") {
-  // console.log('correct - 3 plants killed');
-  answers.push(1);
-   alert('Good job! You are figuring me out.');
-   break;
-  } else if (number > 3){
-    // console.log('incorrect');
-    alert('Hold your horses, my black thumb is not that bad.');
+    alert(userName +', that\'s all for today. You\'re a good sport. Thank you for playing!');
+
   } else {
-    console.log(0)
-    alert('I wish. It was definitely more.');
+    alert('Maybe another time!');
   }
- }
+}
 
- // My favorite sweets
+gameInvite();
 
- alert('One more question, ok? I have a very sweet tooth, and I am wondering if you can guess which treat is my favorite.')
-
- for (var i=0; i<6; i++){
-   var favoriteSweets = ['chocolate truffles', 'peanut butter cups', 'flan', 'hard candy', 'salted carmels', 'cookies', 'donuts', 'bananas'];
-   var sweetGuess = prompt('What do I like most: chocolate truffles, peanut butter cups, flan, hard candy, salted carmels, cookies, donuts, or bananas?');
-   if(sweetGuess === favoriteSweets[4]) {
-    //  console.log('correct - salted carmels are my favorite');
-     answers.push(1);
-     alert('You got it! I would kill for salted carmels right now.');
-     break;
-   } else {
-    //  console.log('incorrect');
-     alert('That\'s not it.')
-   }
-   }
-
-  //  console.log(answers);
-   alert('You\'ve got ' + answers.length + ' correct answers out of 7!');
-
- alert(name +', that\'s all for today. You\'re a good sport. Thank you for playing!')
-
- } else {
-    alert('Maybe another time!'); 
-  }
