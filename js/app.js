f152186c8400e10e900404295c26ba8525484a46
Marta <marta.anthony1@gmail.com>
2020-05-02 18:14:41 -0700

    final final commit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 'use strict';
 
-// greeting
 alert('Oh hello!');
 var userName = prompt('What\'s your name, beautiful?');
-// console.log('User\'s name is ' + name);
+
 alert('Welcome to my page, ' + userName + '!');
 
 var answers = [];
 function husbandQuestion(){
   var husband = prompt('Is my husband a military trained killing machine?'); 
-  if(husband.toLowerCase() === 'yes' || husband.toLowerCase() === 'y'){
-    // console.log('correct annswer - my husband is a killing machine');
+  if(husband.toLowerCase() === 'yes' || husband.toLowerCase() === 'y'){ 
     answers.push(1);
     alert('Yup! Your\'re right. My husband was a captain in the US Marine Corps.');
   } else {
-    //  console.log('incorrect');
     alert('Oh yes he is! He is a badass US Marine Corps officer.');
   }
 }
@@ -23,10 +20,8 @@ function bearQuestion(){
   var bear = prompt('Did I see a polar bear on the street of my hometown in Poland one winter?');
 
   if(bear.toLowerCase() === 'yes' || bear.toLowerCase() === 'y'){
-  // console.log('incorrect');
     alert('No, you silly! Poland is not a Siberian tundra, there are no polar bears on the streets.');
   } else {
-    // console.log('correct - there are no polar bears in Poland');
     answers.push(1);
     alert('Bravo! That would be a scary proposition. And totally messed up, since there are no polar bears outside of the zoo in Poland');
   }
@@ -35,11 +30,9 @@ function veggieQuestion(){
   var favoriteFood = prompt('Is my favorite food group veggies?');
 
   if(favoriteFood.toLowerCase() === 'yes' || favoriteFood.toLowerCase() === 'y'){
-    // console.log('correct - my favorite foods are veggies');
     answers.push(1);
     alert('You bet! I LOVE veggies. I\'m practically a bunny rabbit.');
   } else {
-    // console.log('incorrect');
     alert('Why do you say that? Do you think I\'m fat? Of course I like veggies.');
   }
 }
@@ -48,10 +41,8 @@ function danceQuestion(){
   var dance = prompt('Did I use to dance salsa competitevely?');
 
   if(dance.toLowerCase() === 'yes' || dance.toLowerCase() === 'y'){
-    // console.log('incorrect');
     alert('Heck no, I am the most uncoordinated person ever.');
   } else {
-    // console.log('correct - I can\'t dance');
     answers.push(1);
     alert('Very good. I am not a dancer');
   }
@@ -60,12 +51,10 @@ function plantQuestion(){
   for (var i=0; i<3; i++){
     var number = prompt('How many plants did I accidentally kill last year? Choose a number from 1 to 10. You have 3 guesses!');
     if(parseInt(number) === 3){
-    // console.log('correct - 3 plants killed');
       answers.push(1);
       alert('Good job! You are figuring me out.');
       break;
     } else if (number > 3){
-      // console.log('incorrect');
       alert('Hold your horses, my black thumb is not that bad.');
     } else {
       console.log(0);
@@ -80,12 +69,10 @@ function sweetsQuestion(){
     var favoriteSweets = ['chocolate truffles', 'peanut butter cups', 'flan', 'hard candy', 'salted carmels', 'cookies', 'donuts', 'bananas'];
     var sweetGuess = prompt('What do I like most: chocolate truffles, peanut butter cups, flan, hard candy, salted carmels, cookies, donuts, or bananas?');
     if(sweetGuess === favoriteSweets[4]) {
-    //  console.log('correct - salted carmels are my favorite');
       answers.push(1);
       alert('You got it! I would kill for salted carmels right now.');
       break;
     } else {
-    //  console.log('incorrect');
       alert('That\'s not it.');
     }
   }
@@ -94,16 +81,13 @@ function ageQuestion(){
   var age = prompt('Am I 33?');
 
   if(age.toLowerCase() === 'yes' || age.toLowerCase() === 'y'){
-    // console.log('incorrect');
     alert('Guess again. Just kidding, I\'m not going to tell you my age anyway.');
   } else {
-    // console.log('correct - I am not 33');
     answers.push(1);
     alert('Doesn\'t matter, I won\'t tell you my age anyway.');
   }
 }
 
-// Invitation to play
 function gameInvite(){
   var playGame = prompt('Do you want to play a guessing game with me?');
 
